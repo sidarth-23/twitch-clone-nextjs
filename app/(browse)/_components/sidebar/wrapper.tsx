e3579aa903443bdacaf2f550f95a01414dd3a5a5
Sidarth G <58748190+sidarth-23@users.noreply.github.com>
2023-12-29 01:44:55 +0530

    fixed hydration error in sidebar

--- app/(browse)/_components/sidebar/wrapper.tsx
+++ app/(browse)/_components/sidebar/wrapper.tsx
@@ -1,16 +1,39 @@
-'use client';
+"use client";
 
 import { cn } from "@/lib/utils";
 import { useSidebar } from "@/store/use-sidebar";
+import { useState, useEffect } from "react";
+import { ToggleSkeleton } from "./toggle";
+import { RecommendedSkeleton } from "./recommended";
 
 interface WrapperProps {
   children: React.ReactNode;
 }
 
 export function Wrapper({ children }: WrapperProps) {
+  const [isClient, setIsClient] = useState(false);
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
   const { collapsed } = useSidebar((state) => state);
+  if (!isClient)
+    return (
+      <aside
+        className={cn(
+          "fixed left-0 flex flex-col w-[70px] lg:w-60 h-full bg-background border-r border-[#2D2E35] z-50"
+        )}
+      >
+        <ToggleSkeleton />
+        <RecommendedSkeleton />
+      </aside>
+    );
   return (
-    <aside className={cn("fixed left-0 flex flex-col w-60 h-full bg-background border-r border-[#2D2E35] z-50", collapsed && 'w-[70px]')}>
+    <aside
+      className={cn(
+        "fixed left-0 flex flex-col w-60 h-full bg-background border-r border-[#2D2E35] z-50",
+        collapsed && "w-[70px]"
+      )}
+    >
       {children}
     </aside>
   );
